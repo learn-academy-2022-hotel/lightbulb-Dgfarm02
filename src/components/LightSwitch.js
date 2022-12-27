0931249622201e6d596a26314327e4cb73c22842
dgfarm02 <dgfarm2@gmail.com>
2022-12-27 10:41:26 -0800

    finished the setup branch user stories

--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
+
 
 const LightSwitch = () => {
+  const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
+  const [backgroundColor, setBackgroundColor] = useState("white")
+  const handleClick = () => {
+    setLightSwitchStatus(lightSwitchStatus === "off" ? "on" : "off")
+    setBackgroundColor(backgroundColor === "white" ? "yellow" : "white")
+    // if (lightSwitchStatus === "off") {
+    //   setLightSwitchStatus = "on"
+    // if (lightSwitchStatus === "on") {
+    //   setLightSwitchStatus = "off"
+    // }
+    // }
+  }
   return (
     <>
-      <div className="lightbulb"></div>
+      <div 
+      className="lightbulb"
+      onClick={handleClick}
+      style={{ backgroundColor: backgroundColor}}
+      >{lightSwitchStatus}
+      </div>
     </>
   )
 }
